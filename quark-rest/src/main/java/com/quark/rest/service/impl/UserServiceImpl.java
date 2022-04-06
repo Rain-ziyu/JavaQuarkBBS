1cb83e60d6c53e8c1bd0d13301d18d1db8e4fd6b
wwl <wwl@test.com>
2022-04-06 14:43:20 +0800

    修复登陆退出以及头像框

--- quark-rest/src/main/java/com/quark/rest/service/impl/UserServiceImpl.java
+++ quark-rest/src/main/java/com/quark/rest/service/impl/UserServiceImpl.java
@@ -27,13 +27,15 @@ public class UserServiceImpl extends BaseServiceImpl<UserDao, User> implements U
     private RedisService<Integer> redisSocketService;
 
     @Autowired
-    private RedisService<User> redisService;
+    private RedisService redisService;
 
     @Value("${REDIS_USERID_KEY}")
     private String REDIS_USERID_KEY;
 
     @Value("${REDIS_USER_KEY}")
     private String REDIS_USER_KEY;
+    @Value("${REDIS_USER_TOKEN}")
+    private String REDIS_USER_TOKEN;
 
     @Value("${REDIS_USER_TIME}")
     private Integer REDIS_USER_TIME;
@@ -69,16 +71,24 @@ public class UserServiceImpl extends BaseServiceImpl<UserDao, User> implements U
 
     @Override
     public String LoginUser(User user) {
+        boolean b = redisService.stringHasKey(REDIS_USER_TOKEN + user.getId());
         String token = UUID.randomUUID().toString();
+        if(b == false){
         redisService.cacheString(REDIS_USER_KEY + token, user, REDIS_USER_TIME);
+//        设置一个redis缓存，用来存储token的对应关系
+        redisService.cacheString(REDIS_USER_TOKEN + user.getId(),token, REDIS_USER_TIME);
         redisSocketService.cacheSet(REDIS_USERID_KEY,user.getId());
+        }else {
+//            如果登陆过获取曾经的token
+            token = (String) redisService.getString(REDIS_USER_TOKEN + user.getId());
+        }
 //        loginId.add(user.getId());//维护一个登录用户的set
         return token;
     }
 
     @Override
     public User getUserByToken(String token) {
-        User user = redisService.getStringAndUpDate(REDIS_USER_KEY + token, REDIS_USER_TIME);
+        User user = (User) redisService.getStringAndUpDate(REDIS_USER_KEY + token, REDIS_USER_TIME);
         return user;
     }
 
@@ -86,13 +96,14 @@ public class UserServiceImpl extends BaseServiceImpl<UserDao, User> implements U
     public void LogoutUser(String token) {
         User user = getUserByToken(token);
         redisService.deleteString(REDIS_USER_KEY + token);
+        redisService.deleteString(REDIS_USER_TOKEN + user.getId());
         redisSocketService.deleteSet(REDIS_USERID_KEY,user.getId());
 //        loginId.remove(user.getId());//维护一个登录用户的set
     }
 
     @Override
     public void updateUser(String token, String username, String signature, Integer sex) {
-        User cacheuser = redisService.getString(REDIS_USER_KEY + token);
+        User cacheuser = (User) redisService.getString(REDIS_USER_KEY + token);
         if (cacheuser == null) throw new ServiceProcessException("session过期,请重新登录");
         User user = repository.getOne(cacheuser.getId());
         if(username!=""){
@@ -110,7 +121,7 @@ public class UserServiceImpl extends BaseServiceImpl<UserDao, User> implements U
 
     @Override
     public void updataUserIcon(String token, String icon) {
-        User cacheuser = redisService.getString(REDIS_USER_KEY + token);
+        User cacheuser = (User) redisService.getString(REDIS_USER_KEY + token);
         if (cacheuser == null)
             throw new ServiceProcessException("用户Session过期，请重新登录");
         User user = repository.findById(cacheuser.getId()).get();
@@ -122,7 +133,7 @@ public class UserServiceImpl extends BaseServiceImpl<UserDao, User> implements U
 
     @Override
     public void updateUserPassword(String token, String oldpsd, String newpsd) {
-        User cacheuser = redisService.getString(REDIS_USER_KEY + token);
+        User cacheuser = (User) redisService.getString(REDIS_USER_KEY + token);
         if (cacheuser == null)
             throw new ServiceProcessException("用户Session过期，请重新登录");
         User user = repository.getOne(cacheuser.getId());
